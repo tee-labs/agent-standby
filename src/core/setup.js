e545301a891b9cc47821cb4bf9cc264c7042ac9a
Choi Hiu Kin <mc02cxj@gmail.com>
2026-05-06 13:55:15 +0800

    Refactor getHomeDir to use os.homedir
    Commented out the previous home directory retrieval logic in favor of using os.homedir().

--- src/core/setup.js
+++ src/core/setup.js
@@ -34,11 +34,11 @@ function resolveConfigDir(agentType) {
 }
 
 function getHomeDir() {
-  if (process.env.HOME) return process.env.HOME;
-  if (process.env.USERPROFILE) return process.env.USERPROFILE;
-  if (process.env.HOMEDRIVE && process.env.HOMEPATH) {
-    return process.env.HOMEDRIVE + process.env.HOMEPATH;
-  }
+  // if (process.env.HOME) return process.env.HOME;
+  // if (process.env.USERPROFILE) return process.env.USERPROFILE;
+  // if (process.env.HOMEDRIVE && process.env.HOMEPATH) {
+  //   return process.env.HOMEDRIVE + process.env.HOMEPATH;
+  // }
   return os.homedir();
 }
 
